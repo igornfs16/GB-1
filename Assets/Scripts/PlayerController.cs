5bae191a6cf95d1c3df2737da395320dd7a55423
Administrator <igornfs16@gmail.com>
2021-04-07 22:24:32 +0300

    исправления по замечаниям в ДЗ2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,13 +18,11 @@ public class PlayerController : MonoBehaviour
 
     private float _lastTime;
 
-    //заготовки для вращения камеры
     [SerializeField] private float headMinY = -80f;
     [SerializeField] private float headMaxY = 80f;
     [SerializeField] private float _sensitivity = 1f;
     private float rotationX;
     private float rotationY;
-    private Vector3 _viewDir;
 
     private Vector3 _currentPos;
     [SerializeField] private float _speed = 1;
@@ -32,10 +30,9 @@ public class PlayerController : MonoBehaviour
    
 
 
-    void Start()
+    void Awake()
     {
-        //_currentPos = transform.position;
-        //_viewDir.z = 0;
+
         _gunFirePossition = transform.Find("GunPossition").transform;
         _mortarFirePossition = transform.Find("MortarPossition").transform;
     }
@@ -46,11 +43,6 @@ public class PlayerController : MonoBehaviour
         _currentPos.x = -Input.GetAxis("Horizontal");
         _currentPos.z = -Input.GetAxis("Vertical");
 
-        //знаю что поворот реализован не правильно, но это заплатка, чтобы побродить по уровню :)
-        // _currentDir.x = Input.GetAxis("Mouse X") * _rotationSpeed *Time.deltaTime;
-        //_currentDir.y = Input.GetAxis("Mouse Y") * _rotationSpeed *Time.deltaTime;
-        //_viewDir.x = (transform.rotation.eulerAngles.x + _currentDir.y) % 360;
-        //_viewDir.y = (transform.rotation.eulerAngles.y + _currentDir.x) % 360;
 
         rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * _sensitivity;
         rotationY += Input.GetAxis("Mouse Y") * _sensitivity;
@@ -64,9 +56,6 @@ public class PlayerController : MonoBehaviour
     {
         var move = _currentPos * _speed * Time.fixedDeltaTime;
         transform.Translate(move);
-        
-        
-        //transform.rotation = Quaternion.Euler(_viewDir);
     }
 
     void GunFire()
